ac0637275eed7d815e3841f68efb3c26effdc7e1
iahub4metra <buhai.artem7@gmail.com>
2024-10-25 17:27:01 +0100

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { campersReducer } from "./campers/slice";
+import { filtersReducer } from "./filters/slice";
 
 
 
 const rootReducer = combineReducers({
     campers: campersReducer,
+    filters: filtersReducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
