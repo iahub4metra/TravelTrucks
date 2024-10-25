6bcd4608febdc6b8e6c8132593f050823a230e20
iahub4metra <buhai.artem7@gmail.com>
2024-10-25 17:27:40 +0100

    Update Sidebar.tsx

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,29 @@
+import { useDispatch } from "react-redux";
 import Filters from "../Filters/Filters";
 import LocationsSelect from "../LocationsSelect/LocationsSelect";
+import s from "./Sidebar.module.css"
+import { AppDispatch } from "../../redux/store";
+import { useSelector } from "react-redux";
+import { filterSelector, filterTemplateSelector } from "../../redux/filters/selectors";
+import { compareFilters } from "../../redux/filters/operations";
+import { getCampers } from "../../redux/campers/operations";
 
 const Sidebar = () => {
+
+    const dispatch: AppDispatch = useDispatch()
+    const filters = useSelector(filterSelector)
+    const templatefilters = useSelector(filterTemplateSelector)
+
+    const handleClick = () => {
+        dispatch(compareFilters(templatefilters))
+        dispatch(getCampers(filters))
+    }
+
     return (
         <aside>
             <LocationsSelect />
-            <Filters/>
+            <Filters />
+            <button className={s.btnSearch} type="button" onClick={()=> handleClick()}>Search</button>
         </aside>
     );
 }
