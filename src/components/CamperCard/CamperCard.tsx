aff7054f709b27ad6d2febacbd9257326854c2d8
iahub4metra <buhai.artem7@gmail.com>
2024-10-29 17:32:02 +0000

    Update CamperCard.tsx

--- src/components/CamperCard/CamperCard.tsx
+++ src/components/CamperCard/CamperCard.tsx
@@ -1,10 +1,8 @@
 import { Link } from "react-router-dom";
 import { Camper } from "../App/App.types";
 import s from "./CamperCard.module.css"
-import { FaStar } from "react-icons/fa";
-import { PiMapTrifoldThin } from "react-icons/pi";
-import { chooseIcon } from "../../utils/chooseIcon";
 import Features from "./Features";
+import CamperInnerDiv from "./CamperInnerDiv";
 const CamperCard = ({ camper }: { camper: Camper }) => {
     
     const camperFilters = Object.keys(camper).filter(key => {
@@ -15,17 +13,10 @@ const CamperCard = ({ camper }: { camper: Camper }) => {
         <div className={s.innerCard}>
             <img className={s.imageCamper} src={camper.gallery[0].thumb} alt={camper.description} />
             <div>
-                <div className={s.upperContCard}>
-                    <h2>{camper.name}</h2>
-                    <p>€{camper.price.toFixed(2)}</p>
-                </div>
-                <div className={s.ratingAndLocationCont}>
-                    <span><FaStar className={s.starIcon} />{camper.rating}({camper.reviews.length} {camper.reviews.length > 1 ? 'Reviews' : 'Review'})</span>
-                    <span><PiMapTrifoldThin /> {camper.location}</span>
-                </div>
+                <CamperInnerDiv camper={camper} s={s} />
                 <p className={s.camperDesc}>{camper.description}</p>
                 <Features camper={camper}/>
-                <Link to="/catalog/:camperId">Show More</Link>
+                <Link to={`/catalog/${camper.id}`}>Show More</Link>
             </div>
         </div>
     );
