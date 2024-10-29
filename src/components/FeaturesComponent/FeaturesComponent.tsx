f7d16d3d6bd346b7c35e12fc885ee76d414ec254
iahub4metra <buhai.artem7@gmail.com>
2024-10-29 17:32:20 +0000

    Create FeaturesComponent.tsx

---
+++ src/components/FeaturesComponent/FeaturesComponent.tsx
@@ -0,0 +1,30 @@
+import { useLocation } from "react-router-dom";
+import { Camper } from "../App/App.types";
+import Features from "../CamperCard/Features";
+import s from "./FeaturesComponent.module.css"
+
+type CamperKeys = "form" | "length" | "width" | "height" | "tank" | "consumption";
+
+const keys: CamperKeys[] = ["form","length", "width", "height", "tank", "consumption"]
+
+
+const FeaturesComponent = ({camper}: {camper: Camper}) => {
+    
+    return (
+        <div className={s.featuresContOuter}>
+            <Features camper={camper}/>
+            <div>
+                <h3>Vehicle Details</h3>
+                <ul className={s.detailsList}>
+                    {keys.map((key, index) => (
+                        <li key={index}>
+                            <p>{key.charAt(0).toUpperCase()+ key.slice(1)} <span>{camper[key]}</span></p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+}
+ 
+export default FeaturesComponent;
