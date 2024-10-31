193b4e61bb1ba90ba5dd767f308dff3ffef19187
iahub4metra <buhai.artem7@gmail.com>
2024-10-31 14:53:04 +0000

    button instead of navlink

--- src/components/CamperDetails/CamperDetails.tsx
+++ src/components/CamperDetails/CamperDetails.tsx
@@ -2,29 +2,18 @@ import { useSelector } from "react-redux";
 import { camperSelector } from "../../redux/campers/selectors";
 import CamperInnerDiv from "../CamperCard/CamperInnerDiv";
 import s from "./CamperDetails.module.css"
-import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import FeaturesComponent from "../FeaturesComponent/FeaturesComponent";
 import Reviews from "../Reviews/Reviews";
 import Form from "../Form/Form";
 import clsx from "clsx";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Loader from "../Loader/Loader";
 
 
 const CamperDetails = () => {
 
-    const location = useLocation()
-    const navigate = useNavigate()
-    const [activeHash, setActiveHash] = useState(location.hash)
+    const [activeTab, setActiveTab] = useState("features")
     const camper = useSelector(camperSelector)
-
-
-    useEffect(() => {
-        if (!location.hash) {
-            navigate("#features", { replace: true });
-        }
-        setActiveHash(location.hash)
-    }, [location.hash, navigate]);
 
     return (
         <div>
@@ -40,12 +29,12 @@ const CamperDetails = () => {
                     </ul>
                     <p>{camper.description}</p>
                     <div className={s.navCont}>
-                            <NavLink className={clsx(s.navLink, activeHash === "#features" && s.activeNavLink)} to="#features"><h3>Features</h3></NavLink>
-                            <NavLink className={clsx(s.navLink, activeHash === "#reviews" && s.activeNavLink)} to="#reviews"><h3>Reviews</h3></NavLink>
+                            <button className={clsx(s.navLink, activeTab === "features" && s.activeNavLink)} onClick={()=> setActiveTab("features")}><h3>Features</h3></button>
+                            <button className={clsx(s.navLink, activeTab === "reviews" && s.activeNavLink)} onClick={()=> setActiveTab("reviews")}><h3>Reviews</h3></button>
                        </div>
                     <div className={s.lowerBox}>
                         <div className={s.contWithReviewsOrDetails}>
-                            {location.hash === "#reviews" ? <Reviews camper={camper}/> : <FeaturesComponent camper={camper} />}
+                            {activeTab === "reviews" ? <Reviews camper={camper}/> : <FeaturesComponent camper={camper} />}
                         </div>
                         <Form/>
                     </div>
