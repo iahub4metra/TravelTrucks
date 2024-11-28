143054aa96f59d7ccad1f956e5f55d99dc98e05d
iahub4metra <buhai.artem7@gmail.com>
2024-11-28 18:55:37 +0000

    Update selectors.ts

--- src/redux/filters/selectors.ts
+++ src/redux/filters/selectors.ts
@@ -4,3 +4,5 @@ import { RootState } from "../store";
 export const filterSelector = (state: RootState) => state.filters.filter
 
 export const filterTemplateSelector = (state: RootState) => state.filters.template
+
+export const selectIsOpen = (state: RootState) => state.filters.isOpen
