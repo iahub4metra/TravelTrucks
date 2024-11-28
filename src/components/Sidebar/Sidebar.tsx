f05568cbaf284d036df606e5d8c1ed630a195666
iahub4metra <buhai.artem7@gmail.com>
2024-11-28 18:55:45 +0000

    Update Sidebar.tsx

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,27 +1,34 @@
 import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import clsx from "clsx";
 import Filters from "../Filters/Filters";
 import LocationsSelect from "../LocationsSelect/LocationsSelect";
-import s from "./Sidebar.module.css"
 import { AppDispatch } from "../../redux/store";
-import { useSelector } from "react-redux";
-import { filterSelector } from "../../redux/filters/selectors";
+import { filterSelector, selectIsOpen } from "../../redux/filters/selectors";
 import { getCampers } from "../../redux/campers/operations";
+import { openSidebar } from "../../redux/filters/slice";
+import s from "./Sidebar.module.css"
 
 const Sidebar = () => {
 
+    const isOpen = useSelector(selectIsOpen)
     const dispatch: AppDispatch = useDispatch()
     const filters = useSelector(filterSelector)
 
     const handleClick = async () => {
+        dispatch(openSidebar())
         dispatch(getCampers(filters))
     }
 
     return (
-        <aside>
-            <LocationsSelect />
-            <Filters />
-            <button className={s.btnSearch} type="button" onClick={()=> handleClick()}>Search</button>
-        </aside>
+        <>
+            <aside className={clsx(s.sidebar, {[s.sidebarOpen]: isOpen})}>
+                <LocationsSelect />
+                <Filters />
+                <button className={s.btnSearch} type="button" onClick={()=> handleClick()}>Search</button>
+            </aside>
+        </>
+        
     );
 }
  
