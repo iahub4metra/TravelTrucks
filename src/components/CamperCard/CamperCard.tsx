ae9f9f61a466df91ae9cd5c290c2a550dc34bbae
iahub4metra <buhai.artem7@gmail.com>
2024-11-28 19:57:16 +0000

    Update CamperCard.tsx

--- src/components/CamperCard/CamperCard.tsx
+++ src/components/CamperCard/CamperCard.tsx
@@ -16,7 +16,7 @@ const CamperCard = ({ camper }: { camper: Camper }) => {
                 <CamperInnerDiv camper={camper} s={s} />
                 <p className={s.camperDesc}>{camper.description}</p>
                 <Features camper={camper}/>
-                <Link target="_blank" to={`/catalog/${camper.id}`}>Show More</Link>
+                <a target="_blank" href={`/catalog/${camper.id}`}>Show More</a>
             </div>
         </div>
     );
