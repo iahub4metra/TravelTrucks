0ea46790db0d4ae98c5ba4f360ebda84e654674e
iahub4metra <buhai.artem7@gmail.com>
2024-11-29 23:03:18 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import App from './components/App/App'
@@ -10,9 +9,9 @@ import './index.css'
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>,
 )
