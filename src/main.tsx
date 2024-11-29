0b2163fc664b0875399ba0fd217ab86392a9085a
iahub4metra <buhai.artem7@gmail.com>
2024-11-29 14:24:13 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import App from './components/App/App.js'
+import App from './components/App/App'
 import { store } from './redux/store.js'
 import './index.css'
 
