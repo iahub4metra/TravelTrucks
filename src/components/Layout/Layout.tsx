685850bfa2715b87495505fef5db36ef6f160c1d
iahub4metra <buhai.artem7@gmail.com>
2024-11-29 15:02:27 +0000

    Update Layout.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -13,11 +13,9 @@ const Layout = () => {
             <main>
                 <Suspense fallback={<p>Loading...</p>}>
                     <Routes>
-                        <Route path="/" element={<HomePage />} >
-                            <Route path="catalog" element={<CatalogPage />} />
-                            <Route path="catalog/:camperId" element={<CamperPage />} />
-                        </Route>
-                        
+                        <Route path="/" element={<HomePage/>} />
+                        <Route path="/catalog" element={<CatalogPage />} />
+                        <Route path="/catalog/:camperId" element={<CamperPage />} />
                         <Route path="*" element={<NotFoundPage/>} />
                     </Routes>
                 </Suspense>
