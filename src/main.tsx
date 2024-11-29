580c20a37307b8e977674d85a3271b118c787038
iahub4metra <buhai.artem7@gmail.com>
2024-11-29 15:13:33 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import App from './components/App/App'
-import { store } from './redux/store.js'
+import { store } from './redux/store'
 import './index.css'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
